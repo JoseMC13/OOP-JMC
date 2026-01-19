703e6282cc76333a060ad4a518ff6c98a829a220
JoseMC13 <ppmch2007@gmail.com>
2026-01-19 17:29:43 -0600

    Esta la mayoría del texto. Estan las variables y las designaciones a escoger. Esta el mensaje de incio, el star y el mensaje de salida.

--- java/SCR/edu/jose/mejia/act_1/Main.java
+++ java/SCR/edu/jose/mejia/act_1/Main.java
@@ -5,6 +5,8 @@ import edu.jose.mejia.act_1.ui.CLI;
 public class Main {
 
     public static void main(String[] args){
+        System.out.println("Bienvenido a nuestra concesionaria Honda");
         CLI.start();
+        System.out.println("Esperamos verte pronto.");
     }
 }
